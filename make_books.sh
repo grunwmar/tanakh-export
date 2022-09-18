765b9c74ffcade73b4d2184a6526edd7c36d9e2f
Martin Grunwald <martingrunwald91@gmail.com>
2022-09-18 16:50:20 +0200

    css .verse replaced by table; some pdf result added

--- make_books.sh
+++ make_books.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
+
+#Torah
 bash run.sh pdf source_texts/Torah/1_Bereshit he cs
 bash run.sh pdf source_texts/Torah/2_Shemot he cs
 bash run.sh pdf source_texts/Torah/3_Vayikra he cs
 bash run.sh pdf source_texts/Torah/4_Bemidbar he cs
 bash run.sh pdf source_texts/Torah/5_Devarim he cs
+
+#Neviim
+bash run.sh pdf source_texts/Ktuvim/Daniel he cs
+
+#Ktuvim
+bash run.sh pdf source_texts/Neviim/Yechezkel he cs
+
+# Mishna
+bash run.sh pdf source_texts/Mishna/Avot he en
+bash run.sh pdf source_texts/Mishna/Bava_Kamma he en
